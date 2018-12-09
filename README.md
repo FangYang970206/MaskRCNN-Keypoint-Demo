07186d118bdd8abe2975eb60045320eeda574dcc
FangYang970206 <15270989505@163.com>
2018-12-09 11:53:58 +0800

    fix an error

--- README.md
+++ README.md
@@ -4,12 +4,15 @@ This repo is about Mask RCNN with human-keypoint. More info can be seen in this 
 # Requirements
 * Python 3.4+
 * TensorFlow 1.3+
-* keras 2.0.8
+* keras 2.0.8+
 * numpy, skimage, scipy, Pillow, cython, h5py
 * cocoapi: `pip install "git+https://github.com/philferriere/cocoapi.git#egg=pycocotools&subdirectory=PythonAPI"`
 * cv2: `pip install opencv-python`
 
-# How to run
+# Run in notebook [![Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/FangYang970206/MaskRCNN-Keypoint-Demo/blob/master/demo.ipynb)
+You can open the notebook in google colab, the environment can prepare in one cell and has free GPU.Just enjoy!
+
+# Run in local
 first, clone this repo,
 ```bash
 $ git clone https://github.com/FangYang970206/MaskRCNN-Keypoint-Demo
@@ -18,7 +21,7 @@ then,
 ```bash
 $ cd MaskRCNN-Keypoint-Demo
 ```
-download the **pre-trained model**([baiduyun](https://pan.baidu.com/s/19foQjAu3KSFsIooPuSLAbw), [google drive](https://drive.google.com/open?id=1NvayrcxR9v0kVeH-qRPfQR5mQlbOPQvt)) in the MaskRCNN-Keypoint-Demo folder.
+download the **pre-trained model**([baiduyun](https://pan.baidu.com/s/19foQjAu3KSFsIooPuSLAbw), [google drive](https://drive.google.com/open?id=1NvayrcxR9v0kVeH-qRPfQR5mQlbOPQvt),[dropbox](https://www.dropbox.com/s/5ctrg3br94srrx9/mask_rcnn_coco.h5)) in the MaskRCNN-Keypoint-Demo folder.
 
 finally, you have two choices:
 ```bash
--- main.py
+++ main.py
@@ -76,60 +76,59 @@ def main():
             print("Output file is stored as ", outputFile)
             cv.waitKey(3000)
             break
-        if (not args.image):
-            print("frame shape:", frame.shape)
-            # class_names = ['BG', 'person']
-            results = model.detect_keypoint([frame], verbose=1)
-            r = results[0]
-            if r['masks'].shape[0]:
-                for i in range(r['masks'].shape[2]):
-                    mask = r['masks'][:, :, i]
-                    mask = (mask > maskThreshold)
-                    roi = frame[mask]
-                    colorIndex = random.randint(0, len(colors)-1)
-                    color = colors[colorIndex]
-                    frame[mask] = ([0.3 * color[0],
-                                    0.3 * color[1],
-                                    0.3 * color[2]] + 0.7 * roi).astype(np.uint8)
-                    mask = mask.astype(np.uint8)
-                    _, contours, hierarchy = cv.findContours(mask,
-                                                             cv.RETR_TREE,
-                                                             cv.CHAIN_APPROX_SIMPLE)
-                    cv.drawContours(frame, contours, -1, color, 3,
-                                    cv.LINE_8, hierarchy, 100)
-
-                keypoints = np.array(r['keypoints']).astype(int)
-                skeleton = [0, -1, -1, 5, -1, 6, 5, 7, 6, 8, 7, 9,
-                            8, 10, 11, 13, 12, 14, 13, 15, 14, 16]
-                for i in range(len(keypoints)):
-                    # Skeleton: 11*2
-                    limb_colors = [[0, 0, 255], [0, 170, 255], [0, 255, 170],
-                                   [0, 255, 0], [170, 255, 0], [255, 170, 0],
-                                   [255, 0, 0], [255, 0, 170], [170, 0, 255],
-                                   [170, 170, 0], [170, 0, 170]]
-                    if(len(skeleton)):
-                        skeleton = np.reshape(skeleton, (-1, 2))
-                        neck = np.array((keypoints[i, 5, :]
-                                        + keypoints[i, 6, :]) / 2).astype(int)
-                        if(keypoints[i, 5, 2] == 0 or keypoints[i, 6, 2] == 0):
-                            neck = [0, 0, 0]
-                        limb_index = -1
-                        for limb in skeleton:
-                            limb_index += 1
-                            start_index, end_index = limb
-                            if(start_index == -1):
-                                Joint_start = neck
-                            else:
-                                Joint_start = keypoints[i][start_index]
-                            if(end_index == -1):
-                                Joint_end = neck
-                            else:
-                                Joint_end = keypoints[i][end_index]
-                            if ((Joint_start[2] != 0) & (Joint_end[2] != 0)):
-                                cv.line(frame,
-                                        tuple(Joint_start[:2]),
-                                        tuple(Joint_end[:2]),
-                                        limb_colors[limb_index], 5)
+        
+        print("frame shape:", frame.shape)
+        # class_names = ['BG', 'person']
+        results = model.detect_keypoint([frame], verbose=1)
+        r = results[0]
+        if r['masks'].shape[0]:
+            for i in range(r['masks'].shape[2]):
+                mask = r['masks'][:, :, i]
+                mask = (mask > maskThreshold)
+                roi = frame[mask]
+                colorIndex = random.randint(0, len(colors)-1)
+                color = colors[colorIndex]
+                frame[mask] = ([0.3 * color[0],
+                                0.3 * color[1],
+                                0.3 * color[2]] + 0.7 * roi).astype(np.uint8)
+                mask = mask.astype(np.uint8)
+                _, contours, hierarchy = cv.findContours(mask,
+                                                         cv.RETR_TREE,
+                                                         cv.CHAIN_APPROX_SIMPLE)
+                cv.drawContours(frame, contours, -1, color, 3,
+                                cv.LINE_8, hierarchy, 100)
+            keypoints = np.array(r['keypoints']).astype(int)
+            skeleton = [0, -1, -1, 5, -1, 6, 5, 7, 6, 8, 7, 9,
+                        8, 10, 11, 13, 12, 14, 13, 15, 14, 16]
+            for i in range(len(keypoints)):
+                # Skeleton: 11*2
+                limb_colors = [[0, 0, 255], [0, 170, 255], [0, 255, 170],
+                               [0, 255, 0], [170, 255, 0], [255, 170, 0],
+                               [255, 0, 0], [255, 0, 170], [170, 0, 255],
+                               [170, 170, 0], [170, 0, 170]]
+                if(len(skeleton)):
+                    skeleton = np.reshape(skeleton, (-1, 2))
+                    neck = np.array((keypoints[i, 5, :]
+                                    + keypoints[i, 6, :]) / 2).astype(int)
+                    if(keypoints[i, 5, 2] == 0 or keypoints[i, 6, 2] == 0):
+                        neck = [0, 0, 0]
+                    limb_index = -1
+                    for limb in skeleton:
+                        limb_index += 1
+                        start_index, end_index = limb
+                        if(start_index == -1):
+                            Joint_start = neck
+                        else:
+                            Joint_start = keypoints[i][start_index]
+                        if(end_index == -1):
+                            Joint_end = neck
+                        else:
+                            Joint_end = keypoints[i][end_index]
+                        if ((Joint_start[2] != 0) & (Joint_end[2] != 0)):
+                            cv.line(frame,
+                                    tuple(Joint_start[:2]),
+                                    tuple(Joint_end[:2]),
+                                    limb_colors[limb_index], 5)
         if (args.image):
             cv.imwrite(outputFile, frame.astype(np.uint8))
         else:
